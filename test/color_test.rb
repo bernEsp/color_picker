3fcf946864a5e41c6a18bc088f835268ef24b971
Bernardo Galindo <bernardo466@gmail.com>
2014-07-14 18:24:51 -0500

    test strong colors code generator, the code should be include in the strong colors range, also test a method with the max integer number of colors in the selected palette

--- test/color_test.rb
+++ test/color_test.rb
@@ -6,7 +6,7 @@ module ColorPicker
 		end
 
 		def test_generate
-			assert_match /[0..9]|[a-fA-F]{1,6}/, @color.generate
+			assert_match /([0-9]|[a-fA-F]){5,6}/, @color.generate
 		end
 
 		def test_code
@@ -22,6 +22,21 @@ module ColorPicker
 		def test_strong_colors_palette
 			@color.palette_type("strong_colors")
 			assert_equal [0..7, 0..9, 0..159], @color.palette.range
+		end
+
+		def test_strong_colors_generate_color
+			@color.palette_type("strong_colors")
+			red = (@color.palette.range[0].end+1 * (16**3)).to_s(16).ljust(6, "0")
+			green = (@color.palette.range[1].end+1 * (16**2)).to_s(16).rjust(4, "0") 
+			blue = (@color.palette.range[2].end+1 *16).to_s(16)
+			max_range = red.to_i(16) + green.to_i(16) + blue.to_i(16)
+			colors = []
+			10.times { colors << @color.generate }
+			colors.each do |color|
+				puts color
+				assert_operator color.to_i(16), :<=, (max_range+1)
+			end
+			assert_equal @color.palette.max_number_of_colors, max_range
 		end
 
 		def test_method_missing
