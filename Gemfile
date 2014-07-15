fc42c4d06a92d9c29bf20cf4ea75d19f3f7ed462
Bernardo Galindo <bernardo466@gmail.com>
2014-07-14 23:48:39 -0500

    add coveralls gem and require in test helper

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem "minitest", "~> 5.3.5"
 gem "rake"
+gem "coveralls",	require:false
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ require 'rake'
 require 'color_picker'
 require 'minitest/autorun'
 require 'minitest/unit'
+require 'coveralls'
+Coveralls.wear!
