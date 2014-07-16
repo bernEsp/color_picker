ea5990ca99e4ca03e6d2c5c563ae8a8176f12c1c
Bernardo Galindo <bernardo466@gmail.com>
2014-07-15 23:22:18 -0500

    refacto palette, new method create an instance of Palette, add generate method that recieves options like template and rgb range colors, add sample_color method to return sample of color of the range

--- test/palette_test.rb
+++ test/palette_test.rb
@@ -5,12 +5,20 @@ module ColorPicker
 			@palette = ColorPicker::Palette.new
 		end
 
-		def test_complete_palette
-			assert_equal [0..255, 0 .. 255, 0 .. 255], @palette.complete_palette
+		def test_initialize_complete_range
+			assert_equal [0..0, 0..0, 0..0], @palette.rgb_range
 		end
 
-		def test_strong_colors_palette
-			assert_equal [0..7, 0 .. 9, 0 .. 159], @palette.strong_colors
+		def test_initialize_complete_palette_template
+			assert_equal nil, @palette.template
+		end
+
+		def test_complete_palette
+			assert_equal [0..255, 0 .. 255, 0 .. 255], @palette.generate(template: :complete)
+		end
+
+		def test_strong_paletter
+			assert_equal [0..7, 0 .. 9, 0..255], @palette.generate(template: :strong)
 		end
 
 		def test_to_html
@@ -20,8 +28,27 @@ module ColorPicker
 										 <div style='float=left'>010101<\/div><\/div>/), @palette.to_html 
 		end
 
-		def test_custom_palete
-			assert_equal [0..1, 0..1, 0..1], @palette.custom_palette(0..1, 0..1, 0..1)
+		def test_generate_custom_palette
+			assert_equal [0..1, 0..1, 0..1], @palette.generate(template: :custom, red_color_range: 0..1, green_color_range: 0..1, blue_color_range: 0..1)
+		end
+
+		def test_sample_color
+			assert_match /([0-9]|[a-fA-F]){5,6}/, @palette.sample_color
+		end
+
+		def test_strong_colors_palette_range
+			@palette.generate(type: :strong)
+			max_range = (@palette.red_color_range.end+1) * (@palette.green_color_range.end+1) * (@palette.blue_color_range.end+1)
+			colors = []
+			10.times { colors << @palette.sample_color }
+			colors.each do |color|
+				assert_match (/[0-7]{2}[0-9]{2}[0-9|a-fA-F]{2}/), color
+			end
+			assert_equal @palette.max_number_of_colors, max_range
+		end
+
+		def test_method_missing
+			assert_match "Colors and stars something wrong. Palette types available :complete and :strong via template option", @palette.undefined
 		end
 	end
 end
